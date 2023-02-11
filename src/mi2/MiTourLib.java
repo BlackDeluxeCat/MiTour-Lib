f0e111bf291696a0ce276545fb6783a680fb97cb
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2023-02-11 20:50:39 +0800

    0.2.1

--- src/mi2/MiTourLib.java
+++ src/mi2/MiTourLib.java
@@ -1,10 +1,11 @@
 package mi2;
 
+import arc.util.*;
 import mindustry.mod.*;
 
 public class MiTourLib extends Mod{
-
+    public static final String version = "0.2.1";
     public MiTourLib(){
-
+        Log.infoTag("MTLib", "MiTour Lib " + this.version);
     }
 }
